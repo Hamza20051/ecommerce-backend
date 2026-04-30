ebf89e1d4f2a3b944a43918fd41fe7f409298aef
Hamza20051 <hamzaahmedjabbar@gmail.com>
2026-04-30 14:50:33 +0500

    Update server.js

--- server.js
+++ server.js
@@ -39,6 +39,7 @@ const startServer = async () => {
       credentials: true
       methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     }));
+  app.options('*', cors());
 
     // =========================
     // REQUEST LOGGER
