d15fb87a511074303094a3734c837b819ebb7e33
Hamza20051 <hamzaahmedjabbar@gmail.com>
2026-04-30 14:49:49 +0500

    Update server.js

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ const startServer = async () => {
         'https://www.wearityy.com'
       ],
       credentials: true
+      methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
     }));
 
     // =========================
