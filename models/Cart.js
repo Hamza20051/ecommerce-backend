83a0c6173b21d64210326b35eb0b21c11f851c42
Hamza20051 <hamzaahmedjabbar@gmail.com>
2026-05-04 14:22:36 +0500

    Update Cart.js

--- models/Cart.js
+++ models/Cart.js
@@ -1,29 +1,16 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const CartSchema = new mongoose.Schema(
-  {
-    guestId: {
-      type: String,
-      required: true,
-      index: true,
-    },
-
-    products: [
-      {
-        product: {
-          type: mongoose.Schema.Types.ObjectId,
-          ref: 'Product',
-          required: true,
-        },
-        quantity: {
-          type: Number,
-          default: 1,
-          min: 1,
-        },
+const cartSchema = new mongoose.Schema({
+  guestId: String,
+  items: [
+    {
+      product: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: "Product",
       },
-    ],
-  },
-  { timestamps: true }
-);
+      quantity: Number,
+    },
+  ],
+});
 
-module.exports = mongoose.model('Cart', CartSchema);
+module.exports = mongoose.model("Cart", cartSchema);
