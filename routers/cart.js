379faf4861ee7b07dba8da6ca1a9667ca65ebc0c
Hamza20051 <hamzaahmedjabbar@gmail.com>
2026-05-07 02:03:03 +0500

    Update cart.js

--- routers/cart.js
+++ routers/cart.js
@@ -5,10 +5,27 @@ const {
   addToCart,
   getCart,
   removeFromCart,
+  clearCart,
 } = require("../controllers/cartController");
 
+/* =========================
+   ADD TO CART
+========================= */
 router.post("/:guestId", addToCart);
+
+/* =========================
+   GET CART
+========================= */
 router.get("/:guestId", getCart);
+
+/* =========================
+   REMOVE SINGLE ITEM
+========================= */
 router.delete("/:guestId/:productId", removeFromCart);
+
+/* =========================
+   CLEAR FULL CART
+========================= */
+router.delete("/:guestId", clearCart);
 
 module.exports = router;
